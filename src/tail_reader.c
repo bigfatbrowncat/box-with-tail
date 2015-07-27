f3c861a8df5cc04bbef3a4faf3a0e5fc247844f2
Ilya Mizus <bigfatbrowncat@gmail.com>
2015-07-27 10:52:24 +0300

    Refactored code in reader. Broken something

--- src/tail_reader.c
+++ src/tail_reader.c
@@ -2,33 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAGIC_LENGTH		4
+#define TAIL_MAGIC_LENGTH		4
 
-int main(int argc, char** argv) {
-	FILE* self = fopen(argv[0], "rb");
+#define TAIL_SUCCESS			0
+#define TAIL_NOT_FOUND			1
 
-	fseek(self, -(int)sizeof(size_t) - MAGIC_LENGTH, SEEK_END);
-	size_t data_end = ftell(self);
+size_t tail_data_start, tail_data_length;
+FILE* tail_self;
+
+int init_tail(int argc, char** argv) {
+	tail_self = fopen(argv[0], "rb");
+	
+	fseek(tail_self, -(int)sizeof(size_t) - TAIL_MAGIC_LENGTH, SEEK_END);
+	size_t data_end = ftell(tail_self);
 	
 	// Checking magic
-	char magic[MAGIC_LENGTH];
-	fread(&magic, 1, MAGIC_LENGTH, self);
+	char magic[TAIL_MAGIC_LENGTH];
+	fread(&magic, 1, TAIL_MAGIC_LENGTH, tail_self);
 	if (magic[0] != 'T' || magic[1] != 'A' || magic[2] != 'I' || magic[3] != 'L') {
-		printf("Fatal error: No tail found. Add a tail to proceed.\n");
-		exit(1);
+		//
+		return TAIL_NOT_FOUND;
 	}
-
+	
 	// Reading the base size
-	size_t data_start;
-	fread(&data_start, sizeof(size_t), 1, self);
-	printf("Tail found at 0x%zX\n", data_start);
+	fread(&tail_data_start, sizeof(size_t), 1, tail_self);
+	tail_data_length = data_end - tail_data_start;
 	
+	fclose(tail_self);
+	return TAIL_SUCCESS;
+
+}
+
+size_t get_tail_length() {
+	return tail_data_length;
+}
+
+void read_tail(char* ptr) {
 	// Going to the base-size offset
-	fseek(self, data_start, SEEK_SET);
-	size_t data_length = data_end - data_start;
-	
-	char* data = (char*)malloc(data_length + 1);
-	fread(data, 1, data_length, self);
-	data[data_length] = 0;
-	printf("Tail data: \"%s\" (%ld bytes)\n", data, data_length);
+	fseek(tail_self, tail_data_start, SEEK_SET);
+	fread(ptr, 1, tail_data_length, tail_self);
+}
+
+void free_tail() {
+	fclose(tail_self);
+}
+
+int main(int argc, char** argv) {
+	int res;
+	int itres = init_tail(argc, argv);
+	if (itres == TAIL_SUCCESS) {
+		size_t data_length = get_tail_length();
+		char* data = (char*)malloc(data_length + 1);
+		read_tail(data);
+		data[data_length] = 0;
+		
+		printf("Tail data: \"%s\" (%ld bytes)\n", data, data_length);
+		
+		free(data);
+		res = 0;
+	} else {
+		printf("Fatal error: Eeyore can't find his tail. He's very sad and disappointed\n");
+		res = 1;
+	}
+	free_tail();
 }
